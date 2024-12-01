da82b000a0097261286eda470927a9ca356b17f0
TOBOLL <j.tob@wp.pl>
2024-12-01 19:51:07 +0100

    day one AoC

---
+++ src/main/kotlin/Day01.kt
@@ -0,0 +1,27 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.math.abs
+
+fun main() {
+    val input = readInput()
+    println(partOne(input))
+    println(partTwo(input))
+}
+
+private fun partOne(input: Pair<MutableList<Int>, MutableList<Int>>) =
+    input.first.sorted().zip(input.second.sorted()).sumOf { (elementFormFirst, elementFromSecond) ->
+        abs(elementFormFirst - elementFromSecond)
+    }
+
+private fun partTwo(input: Pair<MutableList<Int>, MutableList<Int>>) =
+    input.first.sumOf { elementFormFirst -> elementFormFirst * input.second.count { elementFormFirst == it } }
+
+
+private fun readInput() =
+    Path("src/main/resources/Day01").readLines()
+        .map { it.split("   ") }
+        .map { it[0].toInt() to it[1].toInt() }
+        .unzip()
+        .let { (first, second) ->
+            first.toMutableList() to second.toMutableList()
+        }
